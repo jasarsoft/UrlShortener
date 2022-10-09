2eefbdef93473881729e27f099362220204ea92d
Edin Jašarević <edinjasar14@gmail.com>
2022-10-09 13:24:31 +0200

    create the url shortener grain

---
+++ UrlShortener/UrlShortenerGrain.cs
@@ -0,0 +1,20 @@
+﻿using Orleans;
+
+namespace UrlShortener
+{
+    public class UrlShortenerGrain : Grain, IUrlShortenerGrain
+    {
+        private KeyValuePair<string, string> _cache;
+
+        public Task SetUrl(string shortenedRouteSegment, string fullUrl)
+        {
+            _cache = new KeyValuePair<string, string>(shortenedRouteSegment, fullUrl);
+            return Task.CompletedTask;
+        }
+
+        public Task<string?> GetUrl()
+        {
+            return Task.FromResult(_cache.Value);
+        }
+    }
+}
