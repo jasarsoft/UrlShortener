8b6b112b9f3cd38725f6edb193522c01332e5924
Edin Jašarević <edinjasar14@gmail.com>
2022-10-09 13:46:24 +0200

    create the endpoint to shorten URLs

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http.Extensions;
 using Orleans;
 using Orleans.Hosting;
 using Orleans.Runtime;
+using UrlShortener;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,5 +16,18 @@ var app = builder.Build();
 var grainFactory = app.Services.GetRequiredService<IGrainFactory>();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/shorten/{*path}",
+    async (IGrainFactory grains, HttpRequest request, string path) =>
+    {
+        var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
+        var shortenerGrain = grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+        await shortenerGrain.SetUrl(shortenedRouteSegment, path);
+        var resultBuilder = new UriBuilder(request.GetEncodedUrl())
+        {
+            Path = $"/go/{shortenedRouteSegment}"
+        };
+
+        return Results.Ok(resultBuilder.Uri);
+    });
 
 app.Run();
