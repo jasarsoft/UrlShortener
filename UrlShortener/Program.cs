ca5c16c8289c10d1e64bfce261775cfaa4d508a6
Edin Jašarević <edinjasar14@gmail.com>
2022-10-09 13:33:53 +0200

    retrive an instance of a grain factory

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -4,7 +4,15 @@ using Orleans.Hosting;
 using Orleans.Runtime;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseOrleans(siloBuilder =>
+{
+    siloBuilder.UseLocalhostClustering();
+});
+
 var app = builder.Build();
+
+var grainFactory = app.Services.GetRequiredService<IGrainFactory>();
 
 app.MapGet("/", () => "Hello World!");
 
